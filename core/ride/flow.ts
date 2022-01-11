554998dfba8a1adf7975b539bce143df183f54b8
gossi <gossi@users.noreply.github.com>
2022-01-11 19:47:52 +0100

    update ride flow

--- core/ride/flow.ts
+++ core/ride/flow.ts
@@ -3,7 +3,7 @@ import { createMachine } from 'xstate';
 import { canAccept, canCancel, canDecline, canFinish, canStart } from './abilities';
 import { Ride, RideState } from './ride';
 
-type RideEvent =
+export type RideEvent =
   | { type: 'ACCEPT' }
   | { type: 'CANCEL' }
   | { type: 'TIME_OUT' }
@@ -11,9 +11,9 @@ type RideEvent =
   | { type: 'DECLINE' }
   | { type: 'FINISH' };
 
-interface RideContext {
+export interface RideContext {
   user: User;
-  ride: Ride;
+  ride?: Ride;
 }
 
 export const RideMachine = createMachine<RideContext, RideEvent>({
@@ -60,19 +60,19 @@ export const RideMachine = createMachine<RideContext, RideEvent>({
 }, {
   guards: {
     canAccept(context) {
-      return canAccept(context.ride, context.user);
+      return Boolean(context.ride && canAccept(context.ride, context.user));
     },
     canCancel(context) {
-      return canCancel(context.ride, context.user);
+      return Boolean(context.ride && canCancel(context.ride, context.user));
     },
     canStart(context) {
-      return canStart(context.ride, context.user);
+      return Boolean(context.ride && canStart(context.ride, context.user));
     },
     canDecline(context) {
-      return canDecline(context.ride, context.user);
+      return Boolean(context.ride && canDecline(context.ride, context.user));
     },
     canFinish(context) {
-      return canFinish(context.ride, context.user);
+      return Boolean(context.ride && canFinish(context.ride, context.user));
     }
   }
 });
